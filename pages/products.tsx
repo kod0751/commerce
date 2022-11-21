a7e0ab481d9d561e33b384040efeb30289037ef7
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-11-21 21:57:25 +0900

    meta data

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,5 +1,6 @@
 // import ImageGallery from 'react-image-gallery';
 
+import Head from 'next/head';
 import Image from 'next/image';
 import Carousel from 'nuka-carousel';
 import { useState } from 'react';
@@ -59,6 +60,25 @@ export default function Products() {
   // return <ImageGallery items={images} />
   return (
     <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
       <Carousel
         animation="fade"
         autoplay
