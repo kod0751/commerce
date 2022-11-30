28f4241ccdabd480e9c1bd6b1337128df91c6723
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-11-30 17:52:48 +0900

    카테고리 구현
    where:  그룹핑 조건

--- prisma/product.ts
+++ prisma/product.ts
@@ -2,19 +2,21 @@ import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+const getRandom = (max: number, min: number) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 const productData: Prisma.productsCreateInput[] = Array.apply(
   null,
-  Array(100)
+  Array(10)
 ).map((_, index) => ({
-  name: `Dark Jean ${index + 1}`,
-  contents: `{"blocks":[{"key":"fngrr","text":"This is a Dark Jean!!${
-    index + 1
-  }","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":10,"length":9,"style":"BOLD"}],"entityRanges":[],"data":{}}],"entityMap":{}}`,
+  name: `Sneakers ${index + 1}`,
+  contents:
+    '{"blocks":[{"key":"fngrr","text":"This is a Sneakers!!","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":10,"length":9,"style":"BOLD"}],"entityRanges":[],"data":{}}],"entityMap":{}}',
   category_id: 1,
-  image_url: `https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/${
-    (index + 1) % 10 === 0 ? 10 : (index + 1) % 10
-  }.jpg`,
-  price: Math.floor(Math.random() * (10000 - 20000) + 20000),
+  image_url:
+    'https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20220304_142%2F1646370635622XhtWM_JPEG%2F47506415345958254_1940472648.jpeg&type=a340',
+  price: getRandom(30000, 10000),
 }));
 
 async function main() {
