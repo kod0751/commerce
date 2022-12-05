d732896b3d6346ebf4dca1ebf393ad6dbc074b9e
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-12-05 23:20:45 +0900

    상품 페이지 UI

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -1,67 +1,95 @@
+import { products } from '@prisma/client';
 import CustomEditor from 'components/Editor';
+import { CATEGORY_MAP } from 'constants/products';
+import { format } from 'date-fns';
 import { convertFromRaw, convertToRaw, EditorState } from 'draft-js';
+import { GetServerSidePropsContext } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Carousel from 'nuka-carousel';
 import { useEffect, useState } from 'react';
 
-export default function Products() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const product = await fetch(
+    `http://localhost:3000/api/get-product?id=${context.params?.id}`
+  )
+    .then((res) => res.json())
+    .then((data) => data.items);
+  return {
+    props: {
+      product: { ...product, images: [product.image_url, product.image_url] },
+    },
+  };
+}
+
+export default function Products(props: {
+  product: products & { images: string[] };
+}) {
   const [index, setIndex] = useState(0);
 
   const router = useRouter();
   const { id: productId } = router.query;
-  const [editorState, setEditorState] = useState<EditorState | undefined>(
-    undefined
+
+  const [editorState] = useState<EditorState | undefined>(() =>
+    props.product.contents
+      ? EditorState.createWithContent(
+          convertFromRaw(JSON.parse(props.product.contents))
+        )
+      : EditorState.createEmpty()
   );
 
-  useEffect(() => {
-    if (productId != null) {
-      fetch(`/api/get-product?id=${productId}`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.items.contents) {
-            setEditorState(
-              EditorState.createWithContent(
-                convertFromRaw(JSON.parse(data.items.contents))
-              )
-            );
-          } else {
-            setEditorState(EditorState.createEmpty());
-          }
-        });
-    }
-  }, [productId]);
+  const product = props.product;
 
   return (
     <>
-      <Carousel
-        animation="fade"
-        autoplay
-        withoutControls
-        wrapAround
-        speed={10}
-        slideIndex={index}
-      >
-        {images.map((item) => (
-          <Image
-            key={item.original}
-            src={item.original}
-            alt="image"
-            width={1000}
-            height={600}
-            layout="responsive"
-          />
-        ))}
-      </Carousel>
-      <div style={{ display: 'flex' }}>
-        {images.map((item, idx) => (
-          <div key={idx} onClick={() => setIndex(idx)}>
-            <Image src={item.original} alt="iamge" width={100} height={60} />
+      {product != null && productId != null ? (
+        <div className="p-24 flex flex-row">
+          <div style={{ maxWidth: 600, marginRight: 52 }}>
+            <Carousel
+              animation="fade"
+              autoplay
+              withoutControls
+              wrapAround
+              speed={10}
+              slideIndex={index}
+            >
+              {product.images.map((url, idx) => (
+                <Image
+                  key={`${url}-carousel-${idx}`}
+                  src={url}
+                  alt="image"
+                  width={600}
+                  height={600}
+                  layout="responsive"
+                />
+              ))}
+            </Carousel>
+            <div className="flex space-x-4 mt-2">
+              {product.images.map((url, idx) => (
+                <div key={`${url}-thumb-${idx}`} onClick={() => setIndex(idx)}>
+                  <Image src={url} alt="iamge" width={100} height={100} />
+                </div>
+              ))}
+            </div>
+            {editorState != null && (
+              <CustomEditor editorState={editorState} readOnly />
+            )}
           </div>
-        ))}
-      </div>
-      {editorState != null && (
-        <CustomEditor editorState={editorState} readOnly />
+          <div style={{ maxWidth: 600 }} className="flex flex-col space-y-6">
+            <div className="text-lg text-zinc-400">
+              {CATEGORY_MAP[product.category_id - 1]}
+            </div>
+            <div className="text-4xl font-semibold">{product.name}</div>
+            <div className="text-lg">
+              {product.price.toLocaleString('ko-kr')}원
+            </div>
+            <div className="text-sm text-zinc-400">
+              등록: {format(new Date(product.createdAt), 'yyyy년 M월 d일')}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>로딩중</div>
       )}
     </>
   );
